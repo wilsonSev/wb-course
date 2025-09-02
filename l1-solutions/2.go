bab22953f02640aac09d04316a872b0230cde00e
Pribavkin Andrei Alexeevich <155998621+wilsonSev@users.noreply.github.com>
2025-09-02 08:47:52 +0300

    Add comments for clarity in printSquare function

--- l1-solutions/2.go
+++ l1-solutions/2.go
@@ -5,18 +5,21 @@ import (
 	"sync"
 )
 
+// функция, возвращающая квадрат числа
 func printSquare(x int, wg *sync.WaitGroup) {
-	defer wg.Done()
+	defer wg.Done() // уменьшаем счетчик на 1
 	fmt.Println(x * x)
 }
 
 func main() {
+	// заданный срез
 	slice := []int{2, 4, 6, 8, 10}
 	wg := sync.WaitGroup{}
-	wg.Add(len(slice))
+	wg.Add(len(slice)) // увеличиваем счетчик на кол-во элементов массива
+	// вызываем по горутине на каждый элемент
 	for i := 0; i < len(slice); i++ {
 		go printSquare(slice[i], &wg)
 	}
-	wg.Wait()
+	wg.Wait() // ждем пока каждая горутина завершит выполнение
 }
 
