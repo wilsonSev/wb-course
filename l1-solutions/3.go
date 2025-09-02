78a8302534bda91500f8eb985909fe6881498361
Pribavkin Andrei Alexeevich <155998621+wilsonSev@users.noreply.github.com>
2025-09-02 08:43:41 +0300

    Update 3.go

--- l1-solutions/3.go
+++ l1-solutions/3.go
@@ -1,7 +1,6 @@
 package main
 
 import (
-	"context"
 	"fmt"
 	"os"
 	"strconv"
@@ -9,45 +8,44 @@ import (
 	"time"
 )
 
-func process(ctx context.Context, c chan int, wg *sync.WaitGroup) {
-	defer wg.Done()
+// функция-воркер
+func process(c chan int, wg *sync.WaitGroup) {
+	defer wg.Done() // в самом конце декрементим wg
 	for {
-		select {
-		case <-ctx.Done():
+		val, ok := <-c // чтение из канала
+		if !ok {       // при ok = false канал закрыт
 			return
-		case val, ok := <-c:
-			if !ok {
-				return
-			}
-			fmt.Println("Processed ", val)
 		}
+		fmt.Println("Processed ", val) // если канал открыт, выводим значение в консоль
 	}
 }
 
 func main() {
+	// проверка, что количество переданных аргументов верное
 	if len(os.Args) < 2 {
 		fmt.Fprintln(os.Stderr, "Ошибка: не передан аргумент")
 		os.Exit(1)
 	}
-	workersNumber, err := strconv.Atoi(os.Args[1])
-	if err != nil {
+	workersNumber, err := strconv.Atoi(os.Args[1]) // конвертируем string в int
+	if err != nil {                                // обрабатываем ошибку приведения типов
 		fmt.Fprintln(os.Stderr, err)
 		os.Exit(1)
 	}
-	c := make(chan int)
-	ctx, cancel := context.WithCancel(context.Background())
-	defer cancel()
+	c := make(chan int) // создаем конал, с которым мы работаем
 
-	iterations := 10
+	iterations := 1000 // количество итераций записи в канал
+	// создаем waitgrouop и добавляем туда кол-во воркеров
 	wg := &sync.WaitGroup{}
 	wg.Add(workersNumber)
+	// запускаем воркеры
 	for i := 0; i < workersNumber; i++ {
-		go process(ctx, c, wg)
+		go process(c, wg)
 	}
+	// пишем в канал
 	for i := 0; i < iterations; i++ {
 		c <- i
 		time.Sleep(time.Millisecond * 50)
 	}
-	close(c)
-	wg.Wait()
+	close(c)  // закрываем канал, чтобы остановить горутины
+	wg.Wait() // ждем, пока все горутины завершат свою работу
 }
